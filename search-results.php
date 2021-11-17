3cc04d65da30894132fadcba351985807e79621c
mauriciosaglimbeni <mauriciosaglimbeni@gmail.com>
2021-11-17 18:42:44 +0100

    update

--- search-results.php
+++ search-results.php
@@ -25,19 +25,20 @@
 <body onLoad = "myFunction()">
 
     <!-- Navbar -->
+     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">FakeyGram</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+      <h4 style="color :#1e69d4;" class="navbar-brand">FakeyGram</h4>
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+       <span class="navbar-toggler-icon"></span>
+     </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="./chats.php">Home <span class="sr-only">(current)</span></a>
+        <a class="navbar-brand" href="./chats.php">Home </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="./logout.php">Logout</a>
+        <a class="navbar-brand" href="./logout.php">Logout</a>
       </li>
       <?php
         $getUser = "SELECT * FROM users WHERE email = '$email'";
