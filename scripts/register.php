8f9a3d265abc4551b755b0453e9d87397653e660
mauriciosaglimbeni <mauriciosaglimbeni@gmail.com>
2021-11-18 17:00:35 +0100

    register now allows pictures

--- scripts/register.php
+++ scripts/register.php
@@ -35,57 +35,51 @@
     if(isset($_POST["submit"])) {
         $check = getimagesize($_FILES["pfp"]["tmp_name"]);
         if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
         } else {
-        echo "File must be an image.";
         $uploadOk = 0;
         }
     }
 
     if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
         $uploadOk = 0;
     }
 
     if ($_FILES["pfp"]["size"] > 500000) {
-        echo "Sorry, your file is too big.";
         $uploadOk = 0;
     }
   
     if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
     && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
     }
 
-    if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
-    } else {
+    if ($uploadOk == 1) {
         if (move_uploaded_file($_FILES["pfp"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["pfp"]["name"]). " has been uploaded.";
-        } else {
-        echo "Sorry, there was an error uploading your file.";
-        }
+    }else{
+        $uploadOk = 0;
     }
+}
 // when fields are not empty
     if($name != "" && $email != "" && $password != "" && $cpassword != "") { 
-        
         $checkUser = "SELECT * FROM users WHERE email = '$email'";
         $checkUserStatus = mysqli_query($conn,$checkUser) or die(mysqli_error($conn));
-
         if(mysqli_num_rows($checkUserStatus) > 0) { 
 
             header('Location: ../index.php?message=You have already registered!');
 
         } else {
-
             if($password == $cpassword) { 
-            
-                $image = basename($_FILES["pfp"]["name"]);
-                $insertUser = "INSERT INTO users(name,email,password,pfp) VALUES('$name','$email','$hashPassword','$image')";
+                if($uploadOk != 0){
+                    $image = basename($_FILES["pfp"]["name"]);
+                    $insertUser = "INSERT INTO users(name,email,password,pfp) VALUES('$name','$email','$hashPassword','$image')";
+                }else{
+                    $insertUser = "INSERT INTO users(name,email,password) VALUES('$name','$email','$hashPassword')";
+                }
+                
                 $insertUserStatus = mysqli_query($conn,$insertUser) or die(mysqli_error($conn));
-    
+
                 if($insertUserStatus) { 
       
                     header('Location: ../index.php?message=You have registered successfully!');
