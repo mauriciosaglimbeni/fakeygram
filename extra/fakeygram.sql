3c226215ca1a1ef4a20c2b044369722f0757b809
mauriciosaglimbeni <mauriciosaglimbeni@gmail.com>
2021-11-18 17:01:52 +0100

    db update

--- extra/fakeygram.sql
+++ extra/fakeygram.sql
@@ -1,14 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 20, 2020 at 11:58 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.3.18
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-11-2021 a las 17:01:14
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.10
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+01:00";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -17,13 +18,13 @@ SET time_zone = "+01:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `fakeygram`
+-- Base de datos: `fakeygram`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `messages`
+-- Estructura de tabla para la tabla `messages`
 --
 
 CREATE TABLE `messages` (
@@ -34,10 +35,18 @@ CREATE TABLE `messages` (
   `createdAt` varchar(255) CHARACTER SET latin1 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `messages`
+--
+
+INSERT INTO `messages` (`id`, `sent_by`, `received_by`, `message`, `createdAt`) VALUES
+(6, 'naruto@gmail.com', 'mauricio@gmail.com', 'Hijo de puta ´ttebayo!', '2021-11-18 04:58:35pm'),
+(7, 'mauricio@gmail.com', 'naruto@gmail.com', 'Tu puta madre chinomierda', '2021-11-18 04:59:16pm');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Estructura de tabla para la tabla `users`
 --
 
 CREATE TABLE `users` (
@@ -45,40 +54,50 @@ CREATE TABLE `users` (
   `email` varchar(255) CHARACTER SET latin1 NOT NULL,
   `name` varchar(255) CHARACTER SET latin1 NOT NULL,
   `password` varchar(255) CHARACTER SET latin1 NOT NULL,
-  `avatar` varchar(255) CHARACTER SET latin1 NOT NULL
+  `pfp` varchar(255) CHARACTER SET latin1 DEFAULT 'default.png'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Indexes for dumped tables
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `name`, `password`, `pfp`) VALUES
+(12, 'adrian@gmail.com', 'Adrian Navarro', '$2y$10$0nVtglYx023wWCSN2ZRID.AnqV5mXno5dhzR0lG.muGA6XDhU0IAO', 'default.png'),
+(14, 'mauricio@gmail.com', 'Mauricio Rodriguez', '$2y$10$UflEDWRIEWCqd9QHJDNbFuZe2AQrBknAf.v6/ZyV0zTxFB3QKPTYO', 'default.png'),
+(19, 'test@gmail.com', 'Testo', '$2y$10$bh1K6GL9MgcY4mKSrcooV.1bptppBt2ZRXH9/pwjE8lrgMSxtyw8i', 'default.png'),
+(24, 'naruto@gmail.com', 'Naruto', '$2y$10$A4VvvW/q7FewG.bZ0mgTjOOJIrvv5a01G6pcdM3b1pRtT0Q5..ZJO', 'naruto.png');
+
+--
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `messages`
+-- Indices de la tabla `messages`
 --
 ALTER TABLE `messages`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Indices de la tabla `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `messages`
+-- AUTO_INCREMENT de la tabla `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
