2630801e33e45d49070535bea2bca7e814d50fab
mauriciosaglimbeni <72515298+mauriciosaglimbeni@users.noreply.github.com>
2021-11-19 23:26:55 +0100

    Update snackbar.php

--- extra/snackbar.php
+++ extra/snackbar.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     if(isset($_GET['message'])){
         $message = $_GET['message'];
@@ -7,16 +6,5 @@
         <?=$message?>
     </div>
 <?php
-    }
-=======
-<?php
-    if(isset($_GET['message'])){
-        $message = $_GET['message'];
-?>
-    <div id="snackbar">
-        <?=$message?>
-    </div>
-<?php
-    }
->>>>>>> 5edd5ba1057bd2fa2ca963b70a83a9ecf6520f8f
+        }
 ?>
